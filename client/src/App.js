672800110c4baf8bf24f1fdbe39d41c571e4da4c
kharit <kharitabderrahim@gmail.com>
2022-08-19 23:15:27 +0000

    fix errors

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,6 @@ import {
 } from "./components/Demandes/";
 
 function App() {
-  console.log(axios);
   const dispatch = useDispatch();
 
   const { isLogin } = useSelector((store) => store.user);
